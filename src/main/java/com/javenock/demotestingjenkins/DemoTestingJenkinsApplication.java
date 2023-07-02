5a368f1e7b0de7602313c2b4f2061c572136d9b9
spar-letta <enockwangila1990@gmail.com>
2023-07-02 16:05:04 +0300

    Testing first java program with jenkins

---
+++ src/main/java/com/javenock/demotestingjenkins/DemoTestingJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.javenock.demotestingjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestingJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTestingJenkinsApplication.class, args);
+		System.out.println("Hello enock and palmer");
+	}
+
+}
---
+++ src/test/java/com/javenock/demotestingjenkins/DemoTestingJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javenock.demotestingjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestingJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
